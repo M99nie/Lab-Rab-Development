9eba4cecfed59f3ac0d8c4fff4fdde27d2b7c17f
M99nie <a615hi03112016@gmail.com>
2024-03-13 15:19:06 +0600

    3
    Добавил текстовое поле и кнопки к фрагменту

--- LabRab1/app/src/main/java/com/example/labrab1/Imya.java
+++ LabRab1/app/src/main/java/com/example/labrab1/Imya.java
@@ -1,9 +1,7 @@
 package com.example.labrab1;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +10,7 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  * Use the {@link Imya#newInstance} factory method to
  * create an instance of this fragment.
+ *
  */
 public class Imya extends Fragment {
 
@@ -23,10 +22,6 @@ public class Imya extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
-    public Imya() {
-        // Required empty public constructor
-    }
 
     /**
      * Use this factory method to create a new instance of
@@ -44,6 +39,10 @@ public class Imya extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public Imya() {
+        // Required empty public constructor
     }
 
     @Override
