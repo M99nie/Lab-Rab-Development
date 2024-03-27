13a07f036ab2a8a60c8c331b99507d17a41452f4
M99nie <a615hi03112016@gmail.com>
2024-03-27 15:17:13 +0600

    Interactive button

--- LabRab1/app/src/main/java/com/example/labrab1/Imya.java
+++ LabRab1/app/src/main/java/com/example/labrab1/Imya.java
@@ -1,63 +1,42 @@
 package com.example.labrab1;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Imya#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
 public class Imya extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Imya.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Imya newInstance(String param1, String param2) {
-        Imya fragment = new Imya();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    TextView textView2;
+    Button button4;
 
     public Imya() {
         // Required empty public constructor
+        super(R.layout.fragment_imya);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_imya, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView textView2 = view.findViewById(R.id.textView2);
+        Button button4= view.findViewById (R.id. button4);
+
+        OnClickListener ocl = new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView2.setText("Button was pressed...");
+                // TODO Auto-generated method stub
+
+            }
+        };
+        button4.setOnClickListener(ocl);
     }
 }
