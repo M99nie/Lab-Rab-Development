b928a15e7c4af2255303f66f5faaa099e4b4d464
M99nie <a615hi03112016@gmail.com>
2024-04-10 15:15:28 +0600

    Создал второй фрагмент для его дальнейшего взаимодействия с первым

---
+++ Lab_Rab_2/app/src/main/java/com/example/lab_rab_2/Tekast.java
@@ -0,0 +1,32 @@
+package com.example.lab_rab_2;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Tekast extends Fragment {
+
+    public Tekast() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tekast, container, false);
+    }
+}
