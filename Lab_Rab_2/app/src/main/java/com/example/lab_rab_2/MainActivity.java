8f49e0c329425c81dd135ad069e1b45656883b66
M99nie <a615hi03112016@gmail.com>
2024-04-10 14:10:11 +0600

    Добавил фрагмент во вторую активити и привязал её вызов к нажатию кнопки с помощью интента

--- Lab_Rab_2/app/src/main/java/com/example/lab_rab_2/MainActivity.java
+++ Lab_Rab_2/app/src/main/java/com/example/lab_rab_2/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
             button.setOnClickListener(new View.OnClickListener() {
 
                 public  void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, Result.class);
+                    Intent intent = new Intent(MainActivity.this, Fragment_View.class);
                     startActivity(intent);
                 }
             });
